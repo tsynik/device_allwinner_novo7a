f541a02f6353211811c8f122e85fadf3ce19d301
nikk gitanes <tsynik@gmail.com>
2012-05-28 21:06:14 +0400

    new vibrator code

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,10 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_CUSTOM_RELEASETOOL := ./device/allwinner/novo7a/releasetools/squisher
 
 BOARD_HAVE_BLUETOOTH := true
-TARGET_USES_CUSTOM_VIBRATOR_PATH := "/sys/class/timed_output/sun4i-vibrator/enable"
+
+#TARGET_USES_CUSTOM_VIBRATOR_PATH := "/sys/class/timed_output/sun4i-vibrator/enable"
+BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/allwinner/common/vibrator.c
+
 BOARD_HAS_SDCARD_INTERNAL := true
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH = "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
