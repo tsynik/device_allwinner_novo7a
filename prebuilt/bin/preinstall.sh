535f6e57f226e7d9152895858f65f196dbc44a71
nikk gitanes <tsynik@gmail.com>
2012-06-15 05:29:25 +0400

    preinstall service

---
+++ prebuilt/bin/preinstall.sh
@@ -0,0 +1,28 @@
+#!/system/xbin/bash
+
+echo "Preinstall job running..."
+BUSYBOX="/system/xbin/busybox"
+
+if [ ! -e /data/system.notfirstrun ]; then		
+	$BUSYBOX touch /data/system.notfirstrun	
+	for f in /data/preinstall/*.apk
+	do
+  		echo "Processing $f file..."
+		/system/bin/sh /system/bin/pm install -r -f $f
+	done
+	# copy android modify tool files
+#	mkdir /mnt/nanda
+#	mount -t vfat /dev/block/nanda /mnt/nanda
+#	$BUSYBOX cp /mnt/nanda/vendor/initlogo.rle /
+#	$BUSYBOX cp /mnt/nanda/vendor/system/build.prop /system/
+#	$BUSYBOX cp /mnt/nanda/vendor/system/media/bootanimation.zip /system/media/
+#	$BUSYBOX cp /mnt/nanda/vendor/system/usr/keylayout/*.kl /system/usr/keylayout/
+#	sync
+#	umount /mnt/nanda
+#	rmdir /mnt/nanda
+
+else
+    $BUSYBOX rm -rf /data/preinstall/*
+fi
+
+echo "Preinstall OK"
